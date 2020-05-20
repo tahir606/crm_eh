aab87a186281f2950b56adebc08c1019db47acac
Tahir <tahir60652@gmail.com>
2020-05-20 13:16:41 +0500

    First Commit

--- config/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  connectionLimit: 100,
-  host: 'localhost',
-  port: 3306,
-  user: 'crm',
-  password: 'crm123!@#',
-  database: 'dist_network'
-};
--- config/email-rec-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  user: 'sales@burhanisolutions.com.pk',
-  password: 'burhanisales',
-  host: 'mail.burhanisolutions.com.pk',
-  port: 143,
-  tls: false
-};
