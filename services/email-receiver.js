e1266934dbb850c2d5abd785626c31f42bef1068
Tahir <tahir60652@gmail.com>
2020-05-21 15:09:58 +0500

    CRM__2020-05-21_(15-09-57)

--- services/email-receiver.js
+++ services/email-receiver.js
@@ -2,12 +2,11 @@ const inspect = require('util').inspect;
 const fs = require('fs');
 const {Base64Decode} = require('base64-stream');
 const Imap = require('imap');
+const simpleParser = require('mailparser').simpleParser;
 
 const config = require('../config/email-rec-config');
 
 var imap = new Imap(config);
-
-
 
 function toUpper(thing) { return thing && thing.toUpperCase ? thing.toUpperCase() : thing;}
 
@@ -55,42 +54,79 @@ function buildAttMessageFunction(attachment) {
   };
 }
 
-imap.once('ready', function() {
-  console.log('Opening Inbox');
-  // 1: Folder Name, 2: false = Read or Write
-  imap.openBox('INBOX', false, function(err, box) {
-    if (err) throw err;
-    imap.search(['UNSEEN'], function(err, results) {
-      if (err) {throw err}
-        try {
-          var f = imap.fetch(results, {
-            bodies: ['HEADER.FIELDS (FROM TO SUBJECT DATE)'],
-            markSeen: false,
-            struct: true
-          });
-        } catch(err) {          
-          console.log(err.message);
-          return;
-        }
+//This handles emails once the box is opened
+var handleBox = function(err, box) {
+  if (err) throw err;
+  imap.search(['UNSEEN'], function(err, results) {
+    if (err) {throw err}
+      try {
+        var f = imap.fetch(results, {
+          bodies: ['HEADER.FIELDS (FROM TO SUBJECT DATE)', ''],
+          markSeen: false,
+          struct: true
+        });
+      } catch(err) {          
+        console.log(err.message);
+        return;
+      }
 
-        f.on('message', function (msg, seqno) {
-          console.log('Message #%d', seqno);
-          var prefix = '(#' + seqno + ') ';
-          msg.on('body', function(stream, info) {
-            var buffer = '';
-            stream.on('data', function(chunk) {
-              buffer += chunk.toString('utf8');
-            });
-            stream.once('end', function() {
-              console.log(buffer);
-              console.log(prefix + 'Parsed header: %s', Imap.parseHeader(buffer));
-            });
-          });
-          msg.once('attributes', function(attrs) {
-            var attachments = findAttachmentParts(attrs.struct);
-            console.log(prefix + 'Has attachments: %d', attachments.length);
-            for (var i = 0, len=attachments.length ; i < len; ++i) {
-              var attachment = attachments[i];
+      f.on('message', function (msg, seqno) {
+        console.log('Message #%d', seqno);
+        var prefix = '(#' + seqno + ') ';
+
+        msg.on('body', function(stream, info) {
+
+
+          var options = {
+            skipHtmlToText: true
+          };
+
+          simpleParser(stream, options)
+          .then(parsed => {
+            console.log(prefix + 'Subject: ' + parsed.subject);
+            console.log(prefix + 'From: ' + parsed.from.text);
+            console.log(prefix + 'To: ' + parsed.to.text);
+            console.log(prefix + 'CC: ' + parsed.cc);
+            console.log(prefix + 'Date: ' + parsed.date);
+            console.log(prefix + 'inReplyTo: ' + parsed.inReplyTo);
+            console.log(prefix + 'Html: ' + parsed.html);
+            console.log(prefix + 'Text: ' + parsed.text);
+            console.log(prefix + 'textAsHtml: ' + parsed.textAsHtml);
+            console.log(prefix + 'Attachments: ' + parsed.attachments);
+          })
+          .catch(err => console.log(err));
+
+
+          // simpleParser(stream).then(function(mail_object) {
+          //       console.log("From:", mail_object.from.value);
+          //       console.log("Subject:", mail_object.subject);
+          //       // console.log("Text body:", mail_object.text);
+          //     }).catch(function(err) {
+          //       console.log('An error occurred:', err.message);
+          //     });
+
+          // var buffer = '', count = 0;
+          // stream.on('data', function(chunk) {
+          //   count += chunk.length;            
+          //   buffer += chunk.toString('utf8');
+          //   if (info.which == 'TEXT') {
+          //     console.log(prefix + 'Body [%s] (%d/%d)', inspect(info.which), count, info.size);
+          //   }
+          // });
+          // stream.once('end', function() {
+          //   // console.log(buffer);
+          //   if (info.which !== 'TEXT')
+          //     console.log(prefix + 'Parsed header: %s', inspect(Imap.parseHeader(buffer)));
+          //   else {
+          //     console.log(prefix + 'Body [%s] Finished', inspect(info.which));             
+          //   }
+          // });
+        });
+        msg.once('attributes', function(attrs) {
+          var attachments = findAttachmentParts(attrs.struct);
+          console.log(prefix + 'Has attachments: %d', attachments.length);
+          for (var i = 0, len=attachments.length ; i < len; ++i) {
+            var attachment = attachments[i];
           /*This is how each attachment looks like {
               partID: '2',
               type: 'application',
@@ -114,19 +150,27 @@ imap.once('ready', function() {
           f.on('message', buildAttMessageFunction(attachment));
         }
       });
-          msg.once('end', function() {
-            console.log(prefix + 'Finished email');
-          });
+        msg.once('end', function() {     
+          console.log(prefix + 'Finished email');
         });
-        f.once('error', function(err) {
-          console.log('Fetch error: ' + err);
-        });
-        f.once('end', function() {
-          console.log('Done fetching all messages!');
-          imap.end();
-        });
-      });    
-  });
+      });
+      f.once('error', function(err) {
+        console.log('Fetch error: ' + err);
+      });
+      f.once('end', function() {
+        console.log('Done fetching all messages!');
+        // Do not end connection
+        imap.end();
+      });
+    });    
+};
+
+imap.once('ready', function() {
+  console.log('Opening Inbox');
+  // 1: Folder Name, 2: false = Read or Write
+  // setInterval(function() {
+    imap.openBox('INBOX', false, handleBox)
+  // } ,3000);  
 });
 
 imap.once('error', function(err) {
